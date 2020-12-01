26decfb0098cc3d9042f713ced3db55efed53cf3
MindaugasSak <m.sakenis@gmail.com>
2020-12-01 17:43:37 +0200

    some bug fix

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import * as S from './Stats.style';
+import { HighlightIdContext } from '../../contexts/HighlightId.context';
 import { HeaderBase } from '../../components';
 import {
   LineChart,
@@ -14,6 +15,9 @@ import {
 function Stats() {
   const [data, setData] = useState([]);
   const token = localStorage.getItem('token');
+  const selectedId = useContext(HighlightIdContext);
+
+  selectedId.setId(3);
 
   useEffect(() => {
     fetch(`${process.env.REACT_APP_SERVER_URL}/stats/users`, {
@@ -34,7 +38,8 @@ function Stats() {
       <S.StatsBox>
         <HeaderBase />
         <S.ChartBox>
-          <LineChart width={600} height={400} data={data}>
+          <h3>New users registered:</h3>
+          <LineChart width={600} height={300} data={data}>
             <CartesianGrid strokeDasharray="5 5" />
             <XAxis dataKey="monthName" />
             <YAxis />
--- src/pages/ViewBooks/ViewBooks.style.js
+++ src/pages/ViewBooks/ViewBooks.style.js
@@ -24,6 +24,7 @@ export const ViewBox = styled.div`
   z-index: 2;
   box-sizing: border-box;
   transform: translate(-50%, -50%);
+  overflow: hidden;
 `;
 
 export const BooksBox = styled.div`
